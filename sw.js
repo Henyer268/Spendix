7a55bf23d23ea9360c68712da6d3d7225b410c1a
Henyer268 <henyermelendezvirguez@inedimacg.edu.co>
2026-05-30 11:13:01 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'spendix-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/dashboard.html',
+  '/movimientos.html',
+  '/categorias.html',
+  '/metas.html',
+  '/ajustes.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
